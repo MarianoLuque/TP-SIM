1797fad1720fafa4f865f1af242c126e56999a09
FedeJulio <82239230+FedeJulio@users.noreply.github.com>
2022-03-19 18:16:01 -0300

    bordecitos

--- TP1/TP1-GeneradorNumerosPseudoaleatorios/TP1-GeneradorNumerosPseudoaleatorios/PantallaInicio.Designer.cs
+++ TP1/TP1-GeneradorNumerosPseudoaleatorios/TP1-GeneradorNumerosPseudoaleatorios/PantallaInicio.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PantallaInicio));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -44,13 +45,16 @@
             this.btn_calcular = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.txt_a = new System.Windows.Forms.TextBox();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(268, 14);
+            this.label1.Location = new System.Drawing.Point(254, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(647, 39);
             this.label1.TabIndex = 0;
@@ -119,7 +123,7 @@
             this.btn_cerrar_programa.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_cerrar_programa.BackgroundImage")));
             this.btn_cerrar_programa.FlatAppearance.BorderSize = 0;
             this.btn_cerrar_programa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_cerrar_programa.Location = new System.Drawing.Point(1126, 12);
+            this.btn_cerrar_programa.Location = new System.Drawing.Point(1127, 8);
             this.btn_cerrar_programa.Name = "btn_cerrar_programa";
             this.btn_cerrar_programa.Size = new System.Drawing.Size(41, 41);
             this.btn_cerrar_programa.TabIndex = 9;
@@ -158,9 +162,9 @@
             // 
             // btn_calcular
             // 
-            this.btn_calcular.Location = new System.Drawing.Point(853, 507);
+            this.btn_calcular.Location = new System.Drawing.Point(926, 469);
             this.btn_calcular.Name = "btn_calcular";
-            this.btn_calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_calcular.Size = new System.Drawing.Size(111, 42);
             this.btn_calcular.TabIndex = 11;
             this.btn_calcular.Text = "Calcular";
             this.btn_calcular.UseVisualStyleBackColor = true;
@@ -185,16 +189,31 @@
             this.txt_a.Size = new System.Drawing.Size(86, 20);
             this.txt_a.TabIndex = 2;
             // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 50;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_cerrar_programa);
+            this.panel1.Location = new System.Drawing.Point(-1, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1189, 56);
+            this.panel1.TabIndex = 12;
+            // 
             // PantallaInicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(1179, 712);
+            this.ClientSize = new System.Drawing.Size(1179, 533);
             this.ControlBox = false;
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btn_calcular);
             this.Controls.Add(this.cmb_a);
-            this.Controls.Add(this.btn_cerrar_programa);
             this.Controls.Add(this.txt_a);
             this.Controls.Add(this.txt_m);
             this.Controls.Add(this.txt_k);
@@ -206,7 +225,6 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -214,8 +232,9 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Generador de Números Pseudoaleatorios";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -238,6 +257,8 @@
         private System.Windows.Forms.Button btn_calcular;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txt_a;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
